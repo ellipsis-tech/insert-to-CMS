fb8dbe232dfdc0de29f37897d616346d05de9135
RicePuddn <ernesthengmengwee@gmail.com>
2024-05-08 10:42:51 +0800

    updated py file

--- insert.py
+++ insert.py
@@ -1,15 +1,15 @@
-from contentful_management import Client
+import contentful_management
 import json
 
-client = Client('gzhf2ey3l7ai','CT3w5uPMUT-cIwFpqMKq_UtbuCRyXYTPwn23QTybk_k')
+client = contentful_management.Client('gzhf2ey3l7ai','CT3w5uPMUT-cIwFpqMKq_UtbuCRyXYTPwn23QTybk_k')
 
 with open('test.json') as f:
     data = json.load(f)
 
-#space = client.spaces().find('gzhf2ey3l7ai')
-environment = client.environments('master')
-content_type = client.content_types('committee','master')
+# prints out the existing entries in contentful committee memebers
+# for item in client.entries({'content_type': 'committee'}):
+#     print(getattr(item,'name'))
 
+# prints out the json file names of committee members
 for item in data:
-    entry = content_type.create(item)
-    entry.publish()
+    print(item['name'])
